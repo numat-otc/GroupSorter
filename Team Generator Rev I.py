01f7eb189d5b1b7bb36a977a42fce10c9d209e0a
numat <numat@stu.otc.school.nz>
2020-10-21 10:59:46 +1300

    1.4 (more comments) and history/presets

--- Team Generator Rev I.py
+++ Team Generator Rev I.py
@@ -6,13 +6,27 @@ import string
 
 #Setup
 VerType =""
-Version = "[V1.4]" # Current Version (Displayed in window title bar to easily recognise different editions)
-os.system("title Team Generator {}{} - A project by Trey".format(VerType,Version)) # Set the window title bar name
-ListYesKey = ["y", "ye", "yes", "yo", "yea", "ya"] # Yes list, check if input is positive response
-global LowerCaseAlphabet
-LowerCaseAlphabet = list(string.ascii_lowercase) # Alphabet to reference input to prevent invalid input
-ListPlayers = [] # Declare empty list of players
+Version = "[V1.4]"                                                                  # Current Version (Displayed in window title bar to easily recognise different editions)
+os.system("title Team Generator {}{} - A project by Trey".format(VerType,Version))  # Set the window title bar name
+ListYesKey = ["y", "ye", "yes", "yo", "yea", "ya"]                                  # Yes list, check if input is positive response
+# Make these variables available everywhere in the code
+global LowerCaseAlphabet                                                            # Make LowerCaseAlphabet accessible anywhere in the code
+global timeout                                                                      # Make timeout accessible anywhere in the code
+LowerCaseAlphabet = list(string.ascii_lowercase)                                    # Alphabet to reference input to prevent invalid input
+timeout = 1                                                                         # The timeout value (in seconds) for when a pause is called
+ListPlayers = []                                                                    # Declare empty list of players
 ##‽TREY‽NUMA‽##
+
+#Check from file for history
+History = open("C:\Users\%username%\AppData\Roaming\TeamGenerator","w+")
+print(History)
+
+
+def TimeOutNormal(): # When code calls a pause
+    time.sleep(timeout)
+
+def TimeOutLong(): # When code calls a longer pause
+    time.sleep(timeout+2)
 
 def TeamSort():                                                                                                     # Sorting Function
     os.system("title Team Generator {}{} - Sorting {} players into teams".format(VerType,Version,len(ListPlayers)))     # Change title
@@ -20,8 +34,18 @@ def TeamSort():                                                                 
         print("> Type the number of teams you would like to sort {} people into ".format(len(ListPlayers)))                 # Get amount of teams
         print("> Type 'cancel' to go back to the main screen")
         NumTeams = (input("|> "))                                                                                           # Input
-        if NumTeams.lower() == "cancel" or NumTeams.lower() == "" or LowerCaseAlphabet in NumTeams.lower():                                                          # If no input
+        if NumTeams.lower() == "cancel":                                                                                    # If no input
             break
+        AlphabetFound = False
+        for i in range(0,26):
+            if LowerCaseAlphabet[i] in NumTeams.lower():
+                AlphabetFound = True
+                break
+        if AlphabetFound == True:
+            print("Error; invalid input, only integers allowed")
+            TimeOutNormal()
+            return
+
         # Account for errors
         try:
             # Try to make NumTeams an integer
@@ -31,13 +55,13 @@ def TeamSort():                                                                 
                 print("Error; You entered a higher amount of teams than there is players")
                 print("Automatically changed amount of teams to {} (maximum amount of teams)".format(len(ListPlayers)))
                 NumTeams = len(ListPlayers)
-                time.sleep(4)
+                TimeOutLong()
             elif NumTeams < 2:
                 if NumTeams == 1:
                     print("Error; Cannot sort into 1 team.")
                 else:
                     print("Error; Cannot sort into {} teams.".format(NumTeams))
-                time.sleep(2)
+                TimeOutNormal()
                 break
 
             # SORTING ALGORITHM 2.0
@@ -75,7 +99,7 @@ def TeamSort():                                                                 
         # Error if integer not given
         except ValueError:
             print("Error; Please enter a valid number (e.g. '{}')".format(random.randint(2, 7)))
-            time.sleep(2)
+            TimeOutNormal()
             break
 
 
@@ -107,21 +131,21 @@ def RemovePlayer(ListPlayers, ListPlayersLOWER):
                     ListPlayers.pop(delete - 1)
                 else:
                     print("Error; name {} does not exist.".format(delete))
-                    time.sleep(2)
+                    TimeOutNormal()
                 break
             except ValueError:
                 print("Error; invalid input.")
-                time.sleep(2)
+                TimeOutNormal()
                 break
 
 def DeleteEndOrStart(p,ListPlayers):
     if len(ListPlayers) > 0:
         ListPlayers.pop(p)
         print("position deleted")
-        time.sleep(1) # Pause for 1 second
+        TimeOutNormal()
     else:
         print("Unable to delete first position")
-        time.sleep(1) # Pause for 1 second
+        TimeOutNormal()
 
 
 # Forever Looping base UI
@@ -168,7 +192,7 @@ while True:
                 TeamSort()
             else:
                 print("Error; There are not enough players to sort.")
-                time.sleep(2)
+                TimeOutNormal()
 
         # Check if wanting to clear
         elif addplayer.lower() == "clear":
@@ -181,17 +205,17 @@ while True:
         # Check if name already exists
         elif ListPlayersLOWER.count(addplayer.lower()) != 0:
             print("Error; player already found")
-            time.sleep(1) # Pause for 1 second
+            TimeOutNormal()
 
         # Prevent unaccounted for printing mess ups
         elif len(addplayer) > 24:
             print("Please enter 24 or less characters.")
-            time.sleep(1) # Pause for 1 second
+            TimeOutNormal()
 
         # Disallow 'cancel' to be added to the player list
         elif addplayer.lower() == "cancel":
             print("Error; Cannot enter name cancel")
-            time.sleep(1) # Pause for 1 second
+            TimeOutNormal()
 
         ### Additional commands that aren't required or as useful, therefore not mentioned to the user
 
@@ -208,6 +232,6 @@ while True:
 
     else: # Else (If no english characters present in input)
         print("Please use at least one character from the English alphabet.")
-        time.sleep(2)
+        TimeOutNormal()
 
 # Return to start of While True statement
