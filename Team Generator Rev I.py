f32f1cb6be1d74f65c977dcd3719d95c0c299c59
treyn <numat@stu.otc.school.nz>
2020-10-29 19:14:54 +1300

    V2.1 Settings/options added along with themes and Autosave and autoload for both settings and players list

--- Team Generator Rev I.py
+++ Team Generator Rev I.py
@@ -7,57 +7,201 @@ import string
 
 # Setup
 VerType = ""
-Version = "[V2.0]"  # Current Version (Displayed in window title bar to easily recognise different editions)
+Version = "[V2.1]"  # Current Version (Displayed in window title bar to easily recognise different editions)
 os.system("title Group Sorter {}{} - A project by Trey".format(VerType, Version))  # Set the window title bar name
 ListYesKey = ["y", "ye", "yes", "yo", "yea", "ya"]  # Yes list, check if input is positive response
 
 global LowerCaseAlphabet  # Make LowerCaseAlphabet accessible anywhere in the code
 global timeout  # Make timeout accessible anywhere in the code
 global ListPlayers  # Make ListPlayers accessible anywhere in the code
+global GoBackList  # Make GoBackList accessible anywhere in the code
+global Themes  # Make Themes accessible anywhere in the code
+global ThemesCode  # Make ThemesCode accessible anywhere in the code
 
 LowerCaseAlphabet = list(string.ascii_lowercase)  # lowercase alphabet to reference input to, to prevent invalid input
 timeout = 1  # The timeout value (in seconds) for when a pause is called
 ListPlayers = []  # Declare empty list of players
-IllegalInput = ["\\n", "back", "cancel","\\"] # Values that the user cannot add to ListPlayers
+GoBackList = ["back", "return", "escape", "cancel"]
+IllegalInput = ["\\n", "back", "back", "return", "escape", "cancel", "\\", "/",
+                "all"]  # Values that the user cannot add to ListPlayers
+Themes = ["Classic", "Dark", "Light", "Yellow", "Blue", "Red", "Green", "Cyan"]
+ThemesCode = ["07", "0F", "F0", "06", "01", "04", "02", "09"]
 
 
-# Automatic loading and saving of names upon opening and closing program
+##‽TREY‽NUMA‽##
+
+def Settings():
+    SettingsTitlePrint = str(("─" * 48) + "\n" + ("─" * 17) + "|| SETTINGS ||" + ("─" * 17) + "\n" + ("─" * 48))
+    os.system("title Group Sorter {}{} - Settings".format(VerType,Version))
+    global Theme
+    global AutoSaveBool
+    while True:
+        AutoSave()
+        while True:
+            os.system("cls")
+            if AutoSaveBool == True:
+                AutoSaveBoolPrint = "On"
+            elif AutoSaveBool == False:
+                AutoSaveBoolPrint = "Off"
+            print(SettingsTitlePrint)
+            print("")
+            print("1- Change Theme (Currently: {})".format(Theme))
+            print("2- Autosave On/Off (Currently: {})".format(AutoSaveBoolPrint))
+            print("─" * 8)
+            print("> Type the number that corresponds to the")
+            print("  setting that you would you like to change")
+            print("> Type 'CANCEL' to go back")
+            SettingsInput = input("|> ")
+            try:
+                SettingsInput = int(SettingsInput)
+                break
+            except:
+                if SettingsInput.lower() in GoBackList:
+                    return
+                print("Error; invalid input.")
+                TimeOutNormal()
+        if SettingsInput == 1:
+            while True:
+                os.system("cls")
+                print(SettingsTitlePrint)
+                print("")
+                print("Change Theme to:")
+                for i in range(0, len(Themes)):
+                    print("{}- {} Mode".format(i + 1, Themes[i]))
+                print("─" * 8)
+                print("> Type the number that corresponds to the")
+                print("  option that you would you like to set")
+                print("> Type 'CANCEL' to go back")
+                SettingsInput = input("|> ")
+                try:
+                    SettingsInput = int(SettingsInput)
+                except:
+                    if SettingsInput.lower() in GoBackList:
+                        break
+                    print("Error; invalid input.")
+                    TimeOutNormal()
+                if SettingsInput > 0 and SettingsInput < len(Themes) + 1:
+                    os.system("color {}".format(ThemesCode[SettingsInput - 1]))
+                    Theme = str(Themes[SettingsInput - 1] + " Mode")
+                    break
+                else:
+                    print("Error; invalid input.")
+                    TimeOutNormal()
+
+        elif SettingsInput == 2:
+            while True:
+                os.system("cls")
+                print(SettingsTitlePrint)
+                print("")
+                print("Change Autosave to:")
+                print("1- On")
+                print("2- Off")
+                print("─" * 8)
+                print("> Type the number that corresponds to the")
+                print("  option that you would you like to set")
+                print("> Type 'CANCEL' to go back")
+                SettingsInput = input("|> ")
+                try:
+                    SettingsInput = int(SettingsInput)
+                except:
+                    if SettingsInput.lower() in GoBackList:
+                        break
+                    print("Error; invalid input.")
+                    TimeOutNormal()
+                if SettingsInput == 1:
+                    AutoSaveBool = True
+                    break
+                elif SettingsInput == 2:
+                    AutoSaveBool = False
+                    break
+                else:
+                    print("Error; invalid input.")
+                    TimeOutNormal()
+
+
+# Automatic loading and saving of names and settings upon opening and closing program
 def AutoLoad():
-    AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()),"r")  # Set document to read and save ListPlayers
+    global Theme
+    global AutoSaveBool
+    # Creat Directory if it doesnt exist
+    try:
+        os.mkdir("C:\\Users\{}\AppData\Roaming\GroupSorter\\".format(os.getlogin()))
+    except:
+        pass
+    ### LOAD PLAYERS
+    try:
+        AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()),
+                           "r")  # Set document to read and save ListPlayers
+    except:
+        AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()), "w")
+        AutoSaveTXT.close()
+        AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()), "r")
     FileRead = AutoSaveTXT.readlines()
-    TXTLines = len(FileRead)
     if len(FileRead) > 1:  # Fix errors when 2 or more names are saved with a "\n"
         for i in range(0, len(FileRead)):
-            if i == TXTLines - 1:
+            if i == len(FileRead) - 1:
                 ListPlayers.append(str(FileRead[i]))
             else:
                 ListPlayers.append(str(FileRead[i])[0:-1])
     else:
         ListPlayers.extend(FileRead)  # Normal load if 1 name only save in text format
-    # print("FileReadList",FileRead)
-    # print("Text file lines (saves)",TXTLines)
-    # print("ListPlayers",ListPlayers)
+
+    ### LOAD SETTINGS
+    try:
+        SettingsTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\settings.txt".format(os.getlogin()), "r")
+    except:
+        SettingsTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\settings.txt".format(os.getlogin()), "w")
+        SettingsTXT.close()
+        SettingsTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()), "r")
+    FileRead = SettingsTXT.readlines()
+    Theme = str("Dark Mode")  # Set Default
+    if len(FileRead) > 1:
+        y = 5
+    else:
+        y = 4
+    if len(FileRead) > 0:
+        for i in range(0, len(Themes)):
+            if (str(FileRead[0])[6:-y]) == Themes[i]:
+                Theme = str((Themes[i]) + " Mode")
+                os.system("color {}".format(ThemesCode[i]))
+    try:
+        if (str(FileRead[1])[9:]) == "False":
+            AutoSaveBool = False
+        else:
+            AutoSaveBool = True
+    except:
+        AutoSaveBool = True  # Set Default
+
     AutoSaveTXT.close()  # Close file (avoid errors as the program does not currently require the association with the text file)
+    SettingsTXT.close()
 
 
 def AutoSave():
-    AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()),"w")  # Set document to read and replace with ListPlayers (even if either are empty)
+    if AutoSaveBool == False:
+        try:
+            os.remove("C:\\Users\{}\AppData\Roaming\GroupSorter\\Latest.txt".format(os.getlogin()))
+        except:
+            pass
+        return
+    AutoSaveTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\Latest.txt".format(os.getlogin()),
+                       "w")  # Set document to read and replace with ListPlayers (even if either are empty)
     for i in range(0, len(ListPlayers)):
         if i == len(ListPlayers) - 1:
             AddLine = str(ListPlayers[i])
         else:
             AddLine = str(ListPlayers[i] + "\n")
         AutoSaveTXT.write(AddLine)
+
+    SettingsTXT = open(r"C:\Users\{}\AppData\Roaming\GroupSorter\settings.txt".format(os.getlogin()), "w")
+    ThemeSave = str(Theme)[:-5]
+    AddLine = str("Theme=" + ThemeSave + "Mode")
+    SettingsTXT.write(AddLine)
+
+    AddLine = str("\nAutoSave=" + str(AutoSaveBool))
+    SettingsTXT.write(AddLine)
+
     AutoSaveTXT.close()
-
-
-try:
-    AutoLoad()
-except:
-    os.mkdir("C:\\Users\{}\AppData\Roaming\GroupSorter\\".format(os.getlogin()))
-
-
-##‽TREY‽NUMA‽##
+    SettingsTXT.close()
 
 
 def TimeOutNormal():  # When code calls a pause
@@ -69,11 +213,11 @@ def TimeOutLong():  # When code calls a longer pause
 
 
 def TeamSort():  # Sorting Function
-    os.system("title Group Sorter {}{} - Sorting {} players into groups".format(VerType,Version,len(ListPlayers)))  # Change title
+    os.system("title Group Sorter {}{} - Sorting {} players into groups".format(VerType, Version,len(ListPlayers)))  # Change title
     while True:  # While True:
         print("> Type the number of groups you would like to sort {} people into ".format(
             len(ListPlayers)))  # Get amount of teams
-        print("> Type 'cancel' to go back to the main screen")
+        print("> Type 'CANCEL' to go back to the main screen")
         NumTeams = (input("|> "))  # Input
         if NumTeams.lower() == "cancel":  # If no input
             break
@@ -94,7 +238,7 @@ def TeamSort():  # Sorting Function
             # solve empty teams or errors
             if NumTeams > len(ListPlayers):
                 print("Error; You entered a higher amount of groups than there is players")
-                print("Automatically changed amount of groups to maximum amount of teams ({len(ListPlayers)})")
+                print("Automatically changed amount of groups to maximum amount of teams ({})".format(len(ListPlayers)))
                 NumTeams = len(ListPlayers)
                 TimeOutLong()
             elif NumTeams < 2:
@@ -127,12 +271,12 @@ def TeamSort():  # Sorting Function
                 "title Group Sorter {}{} - Sorted {} players into {} groups".format(VerType, Version, len(ListPlayers),NumTeams))
             os.system('cls')
             for i in range(0, NumTeams):
-                print("_" * 8)
+                print("─" * 8)
                 print("Team {}: ({})".format(i + 1, NameToTeamList.count(i + 1)))
                 for p in range(0, len(NameToTeamList)):
                     if NameToTeamList[p] == i + 1:
                         print(ListPlayers[p])
-            print("_" * 8)
+            print("─" * 8)
 
             # End of TeamSort, press ENTER to go back to main screen
             input("[ENTER] to skip")
@@ -148,18 +292,18 @@ def TeamSort():  # Sorting Function
 # If removing player desired
 def RemovePlayer(ListPlayersLOWER):
     os.system('cls')
-    print("_" * 8)
+    print("─" * 8)
     print("Players: ({})".format(len(ListPlayers)))
     for i in range(0, len(ListPlayers)):
         print("{}- {}".format(i + 1, ListPlayers[i]))
-    print("_" * 8)
+    print("─" * 8)
     # Which name or name order number would you like to delete sequence
     print("> Type the number that corresponds to the player that you would you like to delete")
     print("> Type the name of the player that you would like to delete")
-    print("> Type 'cancel' to go back to the main screen")
+    print("> Type 'CANCEL' to go back to the main screen")
     delete = (input("|> "))
-    if (delete.lower() == "cancel" or delete.lower() == "back" or delete.lower() == "exit" or delete.lower() == ""):
-        pass
+    if (delete.lower() in GoBackList):
+        return
     elif delete.lower() in ListPlayersLOWER:
         for i in range(0, len(ListPlayers)):
             if delete.lower() in ListPlayersLOWER[i]:
@@ -168,6 +312,7 @@ def RemovePlayer(ListPlayersLOWER):
         while True:
             try:
                 delete = int(delete)
+
                 if delete <= len(ListPlayers) and delete > 0:
                     ListPlayers.pop(delete - 1)
                 else:
@@ -190,7 +335,8 @@ def DeleteEndOrStart(p):
         TimeOutNormal()
 
 
-# Forever Looping base UI
+AutoLoad()  # Startup load
+# Forever looping program
 while True:
     AutoSave()
     if len(ListPlayers) == 1:
@@ -201,17 +347,18 @@ while True:
     # Clear (tidy up) screen (command prompt only)
     os.system('cls')
     # Formatting list, easier to read
-    print("_" * 8)
+    print("─" * 8)
     print("Players: [{}]".format(len(ListPlayers)))
     for i in range(0, len(ListPlayers)):
         print(ListPlayers[i])
     # Separate sections
-    print("_" * 8)
-    print("> type a name and press [ENTER] to add a player to the list")
+    print("─" * 8)
+    print("> Type a name and press [ENTER] to add a player to the list")
     if len(ListPlayers) > 0:
-        print("> type SORT to generate groups")
-        print("> type CLEAR to delete all player names")
-        print("> type REMOVE to delete a specific player")
+        print("> Type 'SORT' to generate groups")
+        print("> Type 'CLEAR' to delete all player names")
+        print("> Type 'REMOVE' to delete a specific player")
+        print("> Type 'SETTINGS' to adjust preferences")
     addplayer = str(input("|> "))
 
     # Check to see if an actual name is input
@@ -243,12 +390,16 @@ while True:
                 print("Error; There are not enough players to sort.")
                 TimeOutNormal()
 
+        # Check if wanting to change settings
+        elif addplayer.lower() == "settings" or addplayer.lower() == "options":
+            Settings()
+
         # Check if wanting to clear
         elif addplayer.lower() == "clear":
             ListPlayers.clear()  # reset list to nul
 
         # Check if wanting to delete a single name
-        elif "remove" in addplayer.lower() or "delete" in addplayer.lower():
+        elif addplayer.lower() == "remove" or addplayer.lower() == "rem" or addplayer.lower() == "delete":
             RemovePlayer(ListPlayersLOWER=ListPlayersLOWER)  # call remove player function
 
         # Check if name already exists
